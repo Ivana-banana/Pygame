8a1239eed040ed8d61f860bcf3aa7be9f3dd9e95
Ivan <Student@student.com>
2024-01-03 10:19:14 +0300

    Добавлены начальное окно и таймер

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ import sys
 
 pygame.init()
 pygame.display.set_caption('Shooting practice')
-size = width, height = 800, 800
+size = width, height = 1000, 600
 screen = pygame.display.set_mode(size)
 FPS = 60
 clock = pygame.time.Clock()
@@ -32,21 +32,22 @@ def load_image(name, colorkey=None):
 
 
 def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
+    intro_text = ["Выберите режим:",
+                  "Точность (нажмите 1 для выбора)",
+                  "Скорость (нажмите 2 для выбора)",
+                  "Реакция (нажмите 3 для выбора)",
+                  "Точность и реакция (нажмите 4 для выбора)"]
 
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
     font = pygame.font.Font(None, 30)
-    text_coord = 50
+    text_coord = 30
     for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('black'))
+        string_rendered = font.render(line, 1, pygame.Color('white'))
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 10
+        intro_rect.x = 500
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
 
@@ -54,24 +55,88 @@ def start_screen():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
+            if event.type == pygame.KEYDOWN:
+                if pygame.key.get_pressed()[pygame.K_1]:
+                    return "accuracy"
+                elif pygame.key.get_pressed()[pygame.K_2]:
+                    return "speed"
+                elif pygame.key.get_pressed()[pygame.K_3]:
+                    return "reaction"
+                elif pygame.key.get_pressed()[pygame.K_4]:
+                    return "accuracy and accuracy"
         pygame.display.flip()
         clock.tick(FPS)
 
 
+def time_from_start(current_time):
+    font = pygame.font.Font(None, 30)
+    text = font.render(f"{(current_time - start_time) / 1000}", True, (0, 0, 0))
+    time_rect = text.get_rect()
+    time_rect.top = 550
+    time_rect.x = 10
+    screen.blit(text, time_rect)
+
+
+def rule(screen):
+    f = pygame.font.Font(None, 20)
+    txt = f.render("Для начала испытания нажмите правую кнопку мыши. Ваша цель - получить максимально\n"
+                   " возможный балл за 30 секунд.", True, (0, 0, 0))
+    r = txt.get_rect()
+    r.top = 10
+    r.x = 10
+    screen.blit(txt, r)
+    txt1 = f.render("Для зачисления балла нажимайте как можно быстрее и как можно точнее на появляющихся монстриков",
+                    True, (0, 0, 0))
+    r = txt1.get_rect()
+    r.top = 40
+    r.x = 10
+    screen.blit(txt1, r)
+
+
+def hide_rule(screen):
+    f_1 = pygame.font.Font(None, 30)
+    txt = f_1.render(f"", True, (0, 0, 0))
+    r = txt.get_rect()
+    r.top = 10
+    r.x = 10
+    screen.blit(txt, r)
+    txt1 = f_1.render("", True, (0, 0, 0))
+    r = txt1.get_rect()
+    r.top = 40
+    r.x = 10
+    screen.blit(txt1, r)
+
+
 if __name__ == '__main__':
-    start_screen()
-    # изображение должно лежать в папке data
+    flag = start_screen()
+    screen = pygame.display.set_mode(size)
+    if flag == "accuracy":
+        pygame.display.set_caption('accuracy')
+    elif flag == "speed":
+        pygame.display.set_caption('speed')
+    elif flag == "reaction":
+        pygame.display.set_caption('reaction')
+    else:
+        pygame.display.set_caption("accuracy and speed")
+    clock = pygame.time.Clock()
     running = True
     fps = 60
-    superman = None
+    key = 0
+    rule(screen)
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        clock.tick(FPS)
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+                start_time = pygame.time.get_ticks()
+                key = 1
+        screen.fill("white")
+        if key == 0:
+            rule(screen)
+        if key == 1:
+            time_from_start(pygame.time.get_ticks())
+            hide_rule(screen)
         pygame.display.flip()
+        clock.tick(FPS)
     pygame.quit()
 
